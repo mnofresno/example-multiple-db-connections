8aebd9abef8a8d7ae27b50517fe79d60ea16e8cc
Mariano Fresno <mnofresno@gmail.com>
2018-01-12 00:31:28 -0300

    initial commit

---
+++ ConsoleApp1/Clase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Clase
+    {
+        private async Task<SqlConnection> GetConnection()
+        {
+            var connection = new SqlConnection("Data Source=.;Initial Catalog=AdventureWorks;Integrated Security=True;MultipleActiveResultSets=true;");
+
+            await connection.OpenAsync();
+
+            return connection;
+        }
+
+        public async Task<List<SqlDataReader>> ExecuteAsync()
+        {
+            var tasksList = new List<Task<SqlDataReader>>();
+            var transactions = new List<SqlTransaction>();
+            var connections = new List<SqlConnection>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var connection = await GetConnection();
+                var command = connection.CreateCommand();
+                var transaction = connection.BeginTransaction();
+                var dr = ExecuteSP(connection, transaction, null);
+                tasksList.Add(dr);
+                transactions.Add(transaction);
+                connections.Add(connection);
+            }
+
+            
+            await Task.WhenAll(tasksList);
+            transactions.ForEach(x => x.Commit());
+            connections.ForEach(x => x.Close());
+
+
+
+            var result = new List<SqlDataReader>();
+
+            foreach (var drTask in tasksList)
+            {
+                var dr = await drTask;
+
+                result.Add(dr);
+            }
+
+            return result;
+        }
+
+        private async Task<SqlDataReader> ExecuteSP(SqlConnection connection, SqlTransaction transaction, SqlCommand command1 = null)
+        {
+            command1 = connection.CreateCommand();
+
+            command1.CommandType = System.Data.CommandType.StoredProcedure;
+            command1.CommandText = "delayCustom";
+            command1.Transaction = transaction;
+            var dr1 = await command1.ExecuteReaderAsync(System.Data.CommandBehavior.Default);
+            return dr1;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clase = new Clase();
+
+            var inicio = DateTime.Now;
+            Console.WriteLine($"INICIO: {inicio}");
+            var task = clase.ExecuteAsync();
+            var result = task.Result;
+            var fin = DateTime.Now;
+            Console.WriteLine($"FIN: {fin}, DURACION: {(fin-inicio).TotalMilliseconds/1000}");
+
+            Thread.Sleep(10000);
+        }
+    }
+}
